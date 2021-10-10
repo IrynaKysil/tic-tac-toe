4e7a99b0c19ad053e0778ea34ed6cccd3c50948c
IrynaK <31361447+IrynaKysil@users.noreply.github.com>
2021-10-09 23:22:02 -0700

    Create checkWin function to check if there is a winning combination

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const O_CLASS = "o";
 const X_CLASS = "x";
+const WINNING_COMBINATIONS = [
+  [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
+];
+
 const board = document.getElementById("board");
 const cells = document.querySelectorAll("[data-cell]");
 let oTurn;
@@ -18,7 +22,14 @@ function onClick(event) {
   //console.log("The cell is clicked!");
   const cell = event.target;
   const currentClass = oTurn ? O_CLASS : X_CLASS;
+
   placeMark(cell, currentClass);
+
+  if (checkWin(currentClass)) {
+    console.log("Winner!")
+  }
+
+
   switchTurns();
   setBoardHoverClass();
 };
@@ -36,3 +47,11 @@ function setBoardHoverClass() {
   board.classList.remove(O_CLASS);
   oTurn ? board.classList.add(O_CLASS) : board.classList.add(X_CLASS);
 };
+
+function checkWin(currentClass) {
+  return WINNING_COMBINATIONS.some(combination => {
+    return combination.every(index => {
+      return cells[index].classList.contains(currentClass)
+    })
+  })
+}
